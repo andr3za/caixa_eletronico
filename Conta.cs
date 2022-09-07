67203b04fb7ebe30cb65e80e560e23e40ae5919b
Andreza Mitterstein Prebianca <102162509+andr3za@users.noreply.github.com>
2022-09-07 19:46:21 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caixa.eletronico
+{
+    public class Conta
+    {
+        public decimal Saldo { get; set; }
+
+        public decimal Deposito { get; set; }
+
+        public decimal Saque { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Caixa.eletronico;
+
+Conta conta = new Conta();
+
+conta.Saldo = 1000;
+string opcao;
+bool menu = true;
+
+Console.WriteLine("Caixa Eletrônico\n");
+
+while (menu) 
+{
+    Console.Clear();
+
+    Console.WriteLine("Menu:\n");
+    Console.WriteLine("1 - Ver Saldo");
+    Console.WriteLine("2 - Depositar");
+    Console.WriteLine("3 - Sacar");
+    Console.WriteLine("4 - Sair\n");
+
+    Console.WriteLine("Digite a opção desejada: ");
+    opcao = Console.ReadLine();
+
+
+    if (opcao == "1")
+    {
+        Console.WriteLine($"Saldo atual é de: {conta.Saldo}\n");
+
+        Console.WriteLine("\nPressione uma tecla para continuar");
+        Console.ReadKey();
+    }
+
+    else if (opcao == "2")
+    {
+        Console.WriteLine("Informe o valor a ser depositado: \n");
+        conta.Deposito = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Saldo atual é de: {conta.Saldo = conta.Saldo + conta.Deposito}\n");
+
+        Console.WriteLine("\nPressione uma tecla para continuar");
+        Console.ReadKey();
+
+    }
+        
+    else if (opcao == "3")
+    {
+
+        Console.WriteLine("Informe o valor do saque:");
+        conta.Saque = Convert.ToInt32(Console.ReadLine());
+
+        if (conta.Saldo < conta.Saque)
+        {
+            Console.WriteLine($"Saldo insuficiente. Seu limite de saque é: {conta.Saldo}\n");
+        }
+        else
+        {
+            Console.WriteLine($"Saque realizado \nSaldo atual é de: {conta.Saldo = conta.Saldo - conta.Saque}\n");
+
+            Console.WriteLine("\nPressione uma tecla para continuar");
+            Console.ReadKey();
+
+        }
+
+
+    }
+    else if (opcao == "4")
+    {
+        menu = false;
+    }
+}
